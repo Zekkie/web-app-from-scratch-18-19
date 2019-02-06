1e9e9f66e85437022923bd9a29bc2c4032162862
Zekkie Berkane <zekkieberkane2@gmail.com>
2019-02-06 14:27:04 +0100

    added functions for filling page with articles

--- week1/script.js
+++ week1/script.js
@@ -1,48 +1,80 @@
 'use-strict'
 
-
-
+//Create subclass from the XHR constructor class
 class ApiCall extends XMLHttpRequest {
-	
+	//manditory for a class to receive params
 	constructor(method,url) {
 	  super(method,url);
 	
 	  this.url = url
-	  this.method = method
-	}
+	  this.method = method;
+	};
 
+
+	//Promise function
 	promise() {
 		return new Promise((resolve, reject) => {
-			this.executor(resolve, reject)
-		})
-	}
-
+			this.executor(resolve, reject);
+		});
+	};
+	//Handling xhr requests
 	executor(resolve, reject) {
 		this.open(this.method,this.url,true);
 		this.onload = (request) => {
 			if(request.currentTarget.status >= 200 && request.currentTarget.status <= 400) {
-				resolve(request.currentTarget.response)
+				resolve(request.currentTarget.response);
+			}else {
+				reject(request.currentTarget.status);
 			}
 		}
 		this.send();
-	}
+	};
 
+
+	//returns the then/catch methods of the promise
 	then(success, fail) {
-		return this.promise().then(success, fail)
-	}
-
+		return this.promise().then(success, fail);
+	};
 	catch(fail) {
 		return this.promise().catch(fail);
-	}
+	};
 
-}
+};
 
-
-let dribbble = new ApiCall("get","https://api.dribbble.com/v2/user?access_token=e351780d7717f3e272a8ee6c3c629217b8a26fd7debe1402cfce1b2f7e0ece23");
+// define new Api call Object
+let dribbble = new ApiCall("get","https://newsapi.org/v2/top-headlines?country=us&apiKey=0edf421b40a64d2fa263ad513a586ddc");
 
 dribbble.then((res) => {
-	console.log(res);
-	return "yo"
-}).then((res) => {
-	console.log(res)
-})
+	let data = JSON.parse(res);
+	return data.articles
+}).then((res) =>{
+	injectPage(res);
+}).catch((err) => {
+	throw new Error("Error: " + err);
+});
+
+
+// lets do something with data
+
+function injectPage(data) {
+	data.forEach((d) => {
+		createArticle(d);
+	});
+};
+
+function createArticle(a) {
+	console.log(a);
+	let body = document.body;
+	let img = new Image();
+	img.src = a.urlToImage
+	let articleBody = document.createElement("article");
+	articleBody.data = a;
+	let titleElement = document.createElement("h1");
+	let titleTextNode = document.createTextNode(a.title);
+
+	titleElement.appendChild(titleTextNode);
+	articleBody.appendChild(img);
+	articleBody.appendChild(titleElement);
+	body.appendChild(articleBody);
+	
+};
