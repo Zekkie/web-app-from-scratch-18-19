0df77bd312fa0fba38fce2529fb7c65fff401eac
Zekkie Berkane <zekkieberkane2@gmail.com>
2019-02-15 14:32:13 +0100

    forced push by teacher

--- week1/script.js
+++ week1/script.js
@@ -1,104 +1,176 @@
 'use-strict'
 
-//Create subclass from the XHR constructor class
-class ApiCall extends XMLHttpRequest {
-	//manditory for a class to receive params
-	constructor(method,url) {
-	  super(method,url);
-	
-	  this.url = url
-	  this.method = method;
+
+
+
+class EasyRequest extends XMLHttpRequest{
+	constructor(target) {
+	  super();
+	  this.target = target;
 	};
-	//Promise function
+
 	promise() {
 		return new Promise((resolve, reject) => {
 			this.executor(resolve, reject);
 		});
 	};
-	//Handling xhr requests
+
 	executor(resolve, reject) {
-		this.open(this.method,this.url,true);
-		this.onload = (request) => {
-			if(request.currentTarget.status >= 200 && request.currentTarget.status <= 400) {
-				resolve(request.currentTarget.response);
+		this.open("GET", this.target, true);
+		this.onload = () => {
+			if(this.status >= 200 && this.status < 400) {
+				resolve(this.response);
 			}else {
-				reject(request.currentTarget.status);
-			}
-		}
+				reject(this.status);
+			};
+		};
+
+		this.onerror = () => {
+			reject(this.response);
+		};
 		this.send();
 	};
-	//returns the then/catch methods of the promise
-	then(success, fail) {
-		return this.promise().then(success, fail);
+
+	then(succes,fail) {
+		return this.promise().then(succes, fail);
 	};
+
 	catch(fail) {
 		return this.promise().catch(fail);
 	};
+
 };
 
 
 
 
+//Create subclass from the XHR constructor class
+class ApiCall extends EasyRequest {
+	//manditory for a class to receive params
+	constructor(url) {
+	  super(url);
 
-let dribbble = new ApiCall("get","https://newsapi.org/v2/top-headlines?country=us&apiKey=0edf421b40a64d2fa263ad513a586ddc");
-
-
-dribbble
-.then((res) => {
-	let data = JSON.parse(res);
-	return data.articles;
-})
-.then((res) =>{
-	injectPage(res);
-})
-.catch((err) => {
-	throw new Error("Error: " + err);
-});
-
-
-// lets do something with data
-function injectPage(data) {
-	data.forEach((d) => {
-		createArticle(d);
-	});
+	};
 };
+
+
+
+//let dribbble = new ApiCall("https://newsapi.org/v2/top-headlines?country=us&apiKey=0edf421b40a64d2fa263ad513a586ddc");
+
+
+// dribbble
+// 	.then((res) => {
+// 		let data = JSON.parse(res);
+// 		return data.articles;
+// 	})
+// 	.then((res) =>{
+		
+// 		return "bla";
+// 	}).then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		throw new Error("Error: " + err);
+// 	});
+
+
+// // lets do something with data
+// function injectPage(data) {
+// 	data.forEach((d) => {
+// 		createArticle(d);
+// 	});
+// };
+
+
+
+
+
+class RenderEngine extends EasyRequest{
+
+	constructor() {
+		super('./templates/home.html');
+	  this.template = '';
+	  this.data = [{title:"Zekkie", content:"blaaa"}];
+	  this.documentTarget = document.body;
+	};
+
+	magicReplacer(text, d) {
+		const regex = new RegExp('\{(.*?)\}')
+		let stripedTemplate = text;
+		while(regex.test(stripedTemplate)) {
+			let found = stripedTemplate.match(regex);
+			stripedTemplate = stripedTemplate.replace(found[0], d[found[1]]);
+		};
+		return stripedTemplate;
+	};
+
+
+	buildDom(template) {
+		let tempStr = '';
+
+		this.data.forEach((d) => {
+			tempStr += this.magicReplacer(template, d);
+		});
+		document.body.insertAdjacentHTML('afterbegin',tempStr);
+	}
+
+	render() {
+		this.then((res) => {
+			this.buildDom(res);
+			//console.log(res)
+		});
+	};
+};
+
+const engine = new RenderEngine  ();
+
+engine.render();
+
 
 
 //build single article
-function createArticle(a) {
-	let body = document.body;
-	let img = new Image();
-	img.src = a.urlToImage
-	let articleBody = document.createElement("article");
-	articleBody.data = a;
-	let titleElement = document.createElement("h1");
-	let titleTextNode = document.createTextNode(a.title);
-	let rmBtn = document.createElement("a");
-	rmBtn.href = "#/" + a.title;
-	rmBtn.innerHTML = "Read more";
+// function createArticle(a) {
+// 	let body = document.body;
+// 	let img = new Image();
+// 	img.src = a.urlToImage
+// 	let articleBody = document.createElement("article");
+// 	articleBody.data = a;
+// 	let titleElement = document.createElement("h1");
+// 	let titleTextNode = document.createTextNode(a.title);
+// 	let rmBtn = document.createElement("a");
+// 	rmBtn.href = "#/" + a.title;
+// 	rmBtn.innerHTML = "Read more";
+// 	titleElement.appendChild(titleTextNode);
+// 	articleBody.appendChild(img).appendChild(titleElement).appendChild(rmBtn);
+// 	body.appendChild(articleBody);
+// };
 
-	titleElement.appendChild(titleTextNode);
-	articleBody.appendChild(img);
-	articleBody.appendChild(titleElement);
-	articleBody.appendChild(rmBtn);
-	body.appendChild(articleBody);
-	
-};
 
-function apiCheck(){
 
-	let date = new Date();
-	if(localStorage.getItem("time")) {
-		let newTime = new Date();
-		let newMS = newTime.getTime();
-		let oldTime = parseInt(localStorage.getItem("time"));
-		console.log((newMS-oldTime)/1000/60);
-	}else {
-		localStorage.setItem("time", date.getTime());
-	}
-	
-	//console.log(date.getTime());
 
-}
 
-apiCheck();
+
+//TODO
+
+// class Cacher {
+// 	constructor() {
+// 	  this.date = new Date();
+// 	  this.now = this.date.getTime();
+// 	  this.cachedTime = 0;
+// 	};
+
+// 	checkTime() {
+// 		this.cachedTime =+ parseInt(localStorage.getItem("time"));
+// 		return (this.now - this.cachedTime) / 1000 / 60;
+// 	};
+
+// 	mutateTimeStamp() {
+// 		if (this.checkTime() > 5) localStorage.setItem("time", this.now);  
+// 	}
+// };
+
+
+
+// let testCacher = new Cacher();
+
+// console.log(testCacher.checkTime());
